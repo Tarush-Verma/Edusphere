87a28ec0afcbfd1b561d5f7bf6e984c5abf2addb
Divyam-Gupta2006 <divyamguptaofficalpostbox@gmail.com>
2025-11-18 02:44:02 +0530

    fixed chod done by mr suryansh

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/navbar";
-import Hero from "./components/hero";
-import Problem from "./components/problem";
-import Solution from "./components/solution";
-import Features from "./components/features";
-import HowItWorks from "./components/howitworks";
-import Screens from "./components/screen";
-import Benefits from "./components/benifits";
-import CTA from "./components/cta";
-import Footer from "./components/footer";
-import Trending from "./components/trending";
-import Mentorship from "./components/mentorship";
-import Internships from "./components/internships";
-import JoinCollege from "./components/joincollege";
-import Freshers from "./components/freshers";
-import Feedback from "./components/feedback";
-import Companies from "./components/companies";
-import About from "./components/about";
 import LandingPage from "./pages/LandingPage.jsx";
 import StudentSignUpPage from "./pages/StudentSignupPage.jsx";
 import StudentLoginPage from "./pages/StudentLoginPage.jsx";
-{/* <div className="bg-white text-gray-800 min-h-screen">
-      <Navbar />
-      <Hero />
-      <Trending />
-      <Mentorship />
-      <Internships />
-      <JoinCollege />
-      <Freshers />
-      <Feedback />
-    <Companies />
-    <About />
-    <Footer />
-    <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/studentsignup" element={<StudentSignUpPage />} />
-        <Route path="/studentlogin" element={<StudentLoginPage />} />
-    </Routes>
-    </div> */}
 
 
 
@@ -54,5 +17,7 @@ export default function App() {
     </Routes>
     </div>
   );
+}
+
 }
 
